c6061c1b50e4cf50c2afca19b333da75c22a2b61
pacblinky <40526207+pacblinky@users.noreply.github.com>
2023-08-20 02:19:31 +0300

    yes

--- popup.js
+++ popup.js
@@ -1,10 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('createAccountN').addEventListener('click', function() {
     chrome.windows.create({ url: ["https://account.proton.me/signup", "https://facebook.com/signup"], incognito: true });
-    chrome.windows.create({ url: ["https://account.proton.me/signup", "https://facebook.com/signup"], incognito: true });
-    chrome.windows.create({ url: ["https://account.proton.me/signup", "https://facebook.com/signup"], incognito: true });
-    chrome.windows.create({ url: ["https://account.proton.me/signup", "https://facebook.com/signup"], incognito: true });
-    chrome.windows.create({ url: ["https://account.proton.me/signup", "https://facebook.com/signup"], incognito: true });
   });
   document.getElementById('createAccountH').addEventListener('click', function() {
     chrome.tabs.create({ url: "https://account.proton.me/signup"});
